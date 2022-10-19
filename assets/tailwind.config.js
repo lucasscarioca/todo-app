c6a6df939483f9c8e8e539058f0e64dad1e3efe0
Lucas Carioca <lucascarioca97@gmail.com>
2022-10-19 10:24:53 -0300

    refactor: adjust todo_live template for tailwind, config style variables and dark/light theme assets usage

--- assets/tailwind.config.js
+++ assets/tailwind.config.js
@@ -10,13 +10,24 @@ module.exports = {
     '../lib/*_web/**/*.*ex'
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        // Dark
+        "dark-bgcolor": "hsl(235, 21%, 11%)",
+        "dark-fg": "hsl(234, 39%, 85%)",
+        "dark-bgtodo": "hsl(235, 24%, 19%)",
+        // Light
+        "bgcolor": "hsl(236, 33%, 92%)",
+        "fg": "hsl(237, 14%, 26%)",
+        "bgtodo": "hsl(0, 0%, 98%)",
+      }
+    },
   },
   plugins: [
     require('@tailwindcss/forms'),
-    plugin(({addVariant}) => addVariant('phx-no-feedback', ['&.phx-no-feedback', '.phx-no-feedback &'])),
-    plugin(({addVariant}) => addVariant('phx-click-loading', ['&.phx-click-loading', '.phx-click-loading &'])),
-    plugin(({addVariant}) => addVariant('phx-submit-loading', ['&.phx-submit-loading', '.phx-submit-loading &'])),
-    plugin(({addVariant}) => addVariant('phx-change-loading', ['&.phx-change-loading', '.phx-change-loading &']))
+    plugin(({ addVariant }) => addVariant('phx-no-feedback', ['&.phx-no-feedback', '.phx-no-feedback &'])),
+    plugin(({ addVariant }) => addVariant('phx-click-loading', ['&.phx-click-loading', '.phx-click-loading &'])),
+    plugin(({ addVariant }) => addVariant('phx-submit-loading', ['&.phx-submit-loading', '.phx-submit-loading &'])),
+    plugin(({ addVariant }) => addVariant('phx-change-loading', ['&.phx-change-loading', '.phx-change-loading &']))
   ]
 }
