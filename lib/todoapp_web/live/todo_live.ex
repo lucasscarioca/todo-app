4ab727109d8e83a88bd8d32028954734a763a6f5
Lucas Carioca <lucascarioca97@gmail.com>
2022-10-21 20:30:40 -0300

    feat: add todos ordering, delete, filter, clear completed and counting functionalities

--- lib/todoapp_web/live/todo_live.ex
+++ lib/todoapp_web/live/todo_live.ex
@@ -4,13 +4,24 @@ defmodule TodoappWeb.TodoLive do
 
   @impl true
   def mount(_params, _session, socket) do
-    {:ok, assign(socket, todo: "", todos: Todos.list_todos())}
+    {:ok,
+     assign(socket,
+       todo: "",
+       todos: order_todos(Todos.list_todos()),
+       todos_left: count_todos_left()
+     )}
   end
 
   @impl true
   def handle_event("submit_todo", %{"new_todo" => todo}, socket) do
     Todos.create_todo(todo)
-    {:noreply, assign(socket, todo: "", todos: Todos.list_todos())}
+
+    {:noreply,
+     assign(socket,
+       todo: "",
+       todos: order_todos(Todos.list_todos()),
+       todos_left: count_todos_left()
+     )}
   end
 
   @impl true
@@ -22,6 +33,67 @@ defmodule TodoappWeb.TodoLive do
   def handle_event("toggle_done", %{"id" => id}, socket) do
     todo = Todos.get_todo!(id)
     Todos.update_todo(todo, %{done: !todo.done})
-    {:noreply, socket}
+
+    {:noreply,
+     assign(socket, todos: order_todos(Todos.list_todos()), todos_left: count_todos_left())}
+  end
+
+  @impl true
+  def handle_event("remove_todo", %{"id" => id}, socket) do
+    todo = Todos.get_todo!(id)
+    Todos.delete_todo(todo)
+    {:noreply, fetch(socket)}
+  end
+
+  @impl true
+  def handle_event("filter_todos", %{"filter" => "all"}, socket) do
+    {:noreply, fetch(socket)}
+  end
+
+  @impl true
+  def handle_event("filter_todos", %{"filter" => filter}, socket) do
+    case filter do
+      "active" ->
+        filtered_todos =
+          Todos.list_todos()
+          |> Enum.filter(fn todo -> todo.done === false end)
+
+        {:noreply, assign(socket, todos: order_todos(filtered_todos))}
+
+      "completed" ->
+        filtered_todos =
+          Todos.list_todos()
+          |> Enum.filter(fn todo -> todo.done === true end)
+
+        {:noreply, assign(socket, todos: order_todos(filtered_todos))}
+
+      _ ->
+        {:error, socket}
+    end
+  end
+
+  @impl true
+  def handle_event("clear_completed", _, socket) do
+    Todos.list_todos()
+    |> Enum.filter(fn todo -> todo.done === true end)
+    |> Enum.each(fn todo -> Todos.delete_todo(todo) end)
+
+    {:noreply, fetch(socket)}
+  end
+
+  defp fetch(socket) do
+    assign(socket, todos: order_todos(Todos.list_todos()))
+  end
+
+  defp count_todos_left() do
+    lefts =
+      Todos.list_todos()
+      |> Enum.filter(fn todo -> todo.done == false end)
+
+    length(lefts)
+  end
+
+  defp order_todos(todos) do
+    Enum.sort_by(todos, &Map.fetch(&1, :id))
   end
 end
